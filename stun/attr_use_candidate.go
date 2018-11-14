8190ce39f18f3d60ce511861184e69fdaf1cb0e6
u5surf <u5.horie@gmail.com>
2018-11-14 15:02:35 +0900

    Fix lint warnings #10

--- stun/attr_use_candidate.go
+++ stun/attr_use_candidate.go
@@ -1,13 +1,16 @@
 package stun
 
+// UseCandidate has no field struct
 type UseCandidate struct {
 }
 
+// Pack message user Candidate
 func (u *UseCandidate) Pack(message *Message) error {
 	message.AddAttribute(AttrUseCandidate, []byte{})
 	return nil
 }
 
+// Unpack message with Candidate
 func (u *UseCandidate) Unpack(message *Message, rawAttribute *RawAttribute) error {
 	return nil
 }
